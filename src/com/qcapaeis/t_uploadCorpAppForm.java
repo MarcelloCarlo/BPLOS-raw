adadea97c487aeb95b4cde359821114bf94d3085
John Carlo Gutierrez <jcgutierrez0102@gmail.com>
2018-08-14 02:54:06 +0800

    Part II upload Function

--- src/com/qcapaeis/t_uploadCorpAppForm.java
+++ src/com/qcapaeis/t_uploadCorpAppForm.java
@@ -13,96 +13,104 @@ import java.util.stream.Collectors;
 @MultipartConfig
 public class t_uploadCorpAppForm extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    
+
     @Override
-    protected void doPost(HttpServletRequest reqX, HttpServletResponse respX) throws ServletException,
-            IOException {
+    protected void doPost(HttpServletRequest reqX, HttpServletResponse respX) throws ServletException, IOException {
 
-        String txtNSingBussName = reqX.getParameter("txtNSingBussName");
-        String txtNSingTaxPayLName = reqX.getParameter("txtNSingTaxPayLName");
-        String txtNSingTaxPayFName = reqX.getParameter("txtNSingTaxPayFName");
-        String txtNSingTaxPayMName = reqX.getParameter("txtNSingTaxPayMName");
-        String txtNSingBussOwnHsNum = reqX.getParameter("txtNSingBussOwnHsNum");
-        String txtNSingBussOwnStrt = reqX.getParameter("txtNSingBussOwnStrt");
-        String txtNSingBussOwnBrgy = reqX.getParameter("txtNSingBussOwnBrgy");
-        String txtNSingBussOwnCity = reqX.getParameter("txtNSingBussOwnCity");
-        String txtNSingBussFlrNo = reqX.getParameter("txtNSingBussFlrNo");
-        String txtNSingBussStrtNo = reqX.getParameter("txtNSingBussStrtNo");
-        String txtNSingBussStrtName = reqX.getParameter("txtNSingBussStrtName");
-        String txtNSingBussBrgyName = reqX.getParameter("txtNSingBussBrgyName");
-        String txtNSingPropIdxNo = reqX.getParameter("txtNSingPropIdxNo");
-        String txtNSingLotBlckNo = reqX.getParameter("txtNSingLotBlckNo");
-        String txtNSingTaxPayTINNo = reqX.getParameter("txtNSingTaxPayTINNo");
-        String txtNSingBussDTIRegNo = reqX.getParameter("txtNSingBussDTIRegNo");
-        String dateNSingBussDTIReg = reqX.getParameter("dateNSingBussDTIReg");
-        String txtNSingBussTelNo = reqX.getParameter("txtNSingBussTelNo");
-        String txtNSingBussFaxNo = reqX.getParameter("txtNSingBussFaxNo");
-        String txtNSingEmpSSSNo = reqX.getParameter("txtNSingEmpSSSNo");
-        String numNSingBussEmpQTY = reqX.getParameter("numNSingBussEmpQTY");
-        String txtNSingBussRepLName = reqX.getParameter("txtNSingBussRepLName");
-        String txtNSingBussRepFName = reqX.getParameter("txtNSingBussRepFName");
-        String txtNSingBussRepMName = reqX.getParameter("txtNSingBussRepMName");
-        String txtNSingBussRepAddr = reqX.getParameter("txtNSingBussRepAddr");
-        String dateNSingBussEstRentStart = reqX.getParameter("dateNSingBussEstRentStart");
-        String numNSingBussEstRentMonth = reqX.getParameter("numNSingBussEstRentMonth");
-        String txtNSingBussEstRentName = reqX.getParameter("txtNSingBussEstRentName");
-        String numNSingBussEstSignbrdArea = reqX.getParameter("numNSingBussEstSignbrdArea");
-        String txtNSingExBuss = reqX.getParameter("txtNSingExBuss");
-        String txtNSingExBussNo = reqX.getParameter("txtNSingExBussNo");
-        String txtNSingExBussLoc = reqX.getParameter("txtNSingExBussLoc");
-        String txtNSingExBuss1 = reqX.getParameter("txtNSingExBuss1");
-        String txtNSingExBussNo1 = reqX.getParameter("txtNSingExBussNo1");
-        String txtNSingExBussLoc1 = reqX.getParameter("txtNSingExBussLoc1");
-        String txtNSingExBuss2 = reqX.getParameter("txtNSingExBuss2");
-        String txtNSingExBussNo2 = reqX.getParameter("txtNSingExBussNo2");
-        String txtNSingExBussLoc2 = reqX.getParameter("txtNSingExBussLoc2");
-        String txtNSingExBuss3 = reqX.getParameter("txtNSingExBuss3");
-        String txtNSingExBussNo3 = reqX.getParameter("txtNSingExBussNo3");
-        String txtNSingExBussLoc3 = reqX.getParameter("txtNSingExBussLoc3");
-        String txtNSBussAct = reqX.getParameter("txtNSBussAct");
-        String numNSingBussUnitNo = reqX.getParameter("numNSingBussUnitNo");
-        String numNSingBussAreaSqmts = reqX.getParameter("numNSingBussAreaSqmts");
-        String numNSingBussCapitalization = reqX.getParameter("numNSingBussCapitalization");
-        String txtNSBussAct1 = reqX.getParameter("txtNSBussAct1");
-        String numNSingBussUnitNo1 = reqX.getParameter("numNSingBussUnitNo1");
-        String numNSingBussAreaSqmts1 = reqX.getParameter("numNSingBussAreaSqmts1");
-        String numNSingBussCapitalization1 = reqX.getParameter("numNSingBussCapitalization1");
-        String txtNSBussAct2 = reqX.getParameter("txtNSBussAct2");
-        String numNSingBussUnitNo2 = reqX.getParameter("numNSingBussUnitNo2");
-        String numNSingBussAreaSqmts2 = reqX.getParameter("numNSingBussAreaSqmts2");
-        String numNSingBussCapitalization2 = reqX.getParameter("numNSingBussCapitalization2");
-        String txtNSBussAct3 = reqX.getParameter("txtNSBussAct3");
-        String numNSingBussUnitNo3 = reqX.getParameter("numNSingBussUnitNo3");
-        String numNSingBussAreaSqmts3 = reqX.getParameter("numNSingBussAreaSqmts3");
-        String numNSingBussCapitalization3 = reqX.getParameter("numNSingBussCapitalization3");
-        String txtNSBussAct4 = reqX.getParameter("txtNSBussAct4");
-        String numNSingBussUnitNo4 = reqX.getParameter("numNSingBussUnitNo4");
-        String numNSingBussAreaSqmts4 = reqX.getParameter("numNSingBussAreaSqmts4");
-        String numNSingBussCapitalization4 = reqX.getParameter("numNSingBussCapitalization4");
-        String txtNSBussAct5 = reqX.getParameter("txtNSBussAct5");
-        String numNSingBussUnitNo5 = reqX.getParameter("numNSingBussUnitNo5");
-        String numNSingBussAreaSqmts5 = reqX.getParameter("numNSingBussAreaSqmts5");
-        String numNSingBussCapitalization5 = reqX.getParameter("numNSingBussCapitalization5");
-        String txtNSBussAct6 = reqX.getParameter("txtNSBussAct6");
-        String numNSingBussUnitNo6 = reqX.getParameter("numNSingBussUnitNo6");
-        String numNSingBussAreaSqmts6 = reqX.getParameter("numNSingBussAreaSqmts6");
-        String numNSingBussCapitalization6 = reqX.getParameter("numNSingBussCapitalization6");
-        String txtNSBussAct7 = reqX.getParameter("txtNSBussAct7");
-        String numNSingBussUnitNo7 = reqX.getParameter("numNSingBussUnitNo7");
-        String numNSingBussAreaSqmts7 = reqX.getParameter("numNSingBussAreaSqmts7");
-        String numNSingBussCapitalization7 = reqX.getParameter("numNSingBussCapitalization7");
+        String txtNCorpBussName = reqX.getParameter("txtNCorpBussName");
+        String txtNCorpTaxPayName = reqX.getParameter("txtNCorpTaxPayName");
+        String txtNCorpPresidentName = reqX.getParameter("txtNCorpPresidentName");
+        String txtNCorpBussFlrNo = reqX.getParameter("txtNCorpBussFlrNo");
+        String txtNCorpBussStrtNo = reqX.getParameter("txtNCorpBussStrtNo");
+        String txtNCorpBussStrtName = reqX.getParameter("txtNCorpBussStrtName");
+        String txtNCorpBussBrgyName = reqX.getParameter("txtNCorpBussBrgyName");
+        String txtNCorpLotBlckNo = reqX.getParameter("txtNCorpLotBlckNo");
+        String txtNCorpAuthRepName = reqX.getParameter("txtNCorpAuthRepName");
+        String txtNCorpAuthRepPos = reqX.getParameter("txtNCorpAuthRepPos");
+        String txtNCorpRepStrtNo = reqX.getParameter("txtNCorpRepStrtNo");
+        String txtNCorpRepStrtName = reqX.getParameter("txtNCorpRepStrtName");
+        String txtNCorpRepBrgyName = reqX.getParameter("txtNCorpRepBrgyName");
+        String txtNCorpRepCity = reqX.getParameter("txtNCorpRepCity");
+        String txtNCorpBussSECRegNo = reqX.getParameter("txtNCorpBussSECRegNo");
+        String dateNCorpBussSECReg = reqX.getParameter("dateNCorpBussSECReg");
+        String txtNCorpTaxPayTINNo = reqX.getParameter("txtNCorpTaxPayTINNo");
+        String txtNCorpTelNo = reqX.getParameter("txtNCorpTelNo");
+        String txtNCorpFaxNo = reqX.getParameter("txtNCorpFaxNo");
+        String txtNCorpEmpSSSNo = reqX.getParameter("txtNCorpEmpSSSNo");
+        String numNCorpEmpQTY = reqX.getParameter("numNCorpEmpQTY");
+        String dateNCorpBussEstRentStart = reqX.getParameter("dateNCorpBussEstRentStart");
+        String numNCorpBussEstRentMonth = reqX.getParameter("numNCorpBussEstRentMonth");
+        String txtNCorpBussEstRentName = reqX.getParameter("txtNCorpBussEstRentName");
+        String numNCorpBussEstSignbrdArea = reqX.getParameter("numNCorpBussEstSignbrdArea");
 
-        Part fileNSingLocSketchEst = reqX.getPart("fileNSingLocSketchEst");
-        Part fileNSingBrgyClear = reqX.getPart("fileNSingBrgyClear");
-        Part fileNSingDTIreg = reqX.getPart("fileNSingDTIreg");
-        Part fileNSingTitleProp = reqX.getPart("fileNSingTitleProp");
-        Part fileNSingLessorBussPermit = reqX.getPart("fileNSingLessorBussPermit");
-        Part fileNSingAuthLetterID = reqX.getPart("fileNSingAuthLetterID");
-        //Hmmm
-        //Part fileNsingOthers = reqX.getPart("fileNsingOthers");
-        List<Part> fileNSingOthers = reqX.getParts().stream().filter(part -> "fileNSingOthers".equals(part.getName())).collect(Collectors.toList());
+        String txtNCorpExBuss = reqX.getParameter("txtNCorpExBuss");
+        String txtNCorpExBussNo = reqX.getParameter("txtNCorpExBussNo");
+        String txtNCorpExBussLoc = reqX.getParameter("txtNCorpExBussLoc");
 
-        //Do the insert here if you can
-        
+        String txtNCorpExBuss1 = reqX.getParameter("txtNCorpExBuss1");
+        String txtNCorpExBussNo1 = reqX.getParameter("txtNCorpExBussNo1");
+        String txtNCorpExBussLoc1 = reqX.getParameter("txtNCorpExBussLoc1");
+
+        String txtNCorpExBuss2 = reqX.getParameter("txtNCorpExBuss2");
+        String txtNCorpExBussNo2 = reqX.getParameter("txtNCorpExBussNo2");
+        String txtNCorpExBussLoc2 = reqX.getParameter("txtNCorpExBussLoc2");
+
+        String txtNCorpExBuss3 = reqX.getParameter("txtNCorpExBuss3");
+        String txtNCorpExBussNo3 = reqX.getParameter("txtNCorpExBussNo3");
+        String txtNCorpExBussLoc3 = reqX.getParameter("txtNCorpExBussLoc3");
+
+        String txtNCorpAct = reqX.getParameter("txtNCorpAct");
+        String numNCorpBussUnitNo = reqX.getParameter("numNCorpBussUnitNo");
+        String numNCorpBussAreaSqmts = reqX.getParameter("numNCorpBussAreaSqmts");
+        String numNCorpBussCapitalization = reqX.getParameter("numNCorpBussCapitalization");
+
+        String txtNCorpAct1 = reqX.getParameter("txtNCorpAct1");
+        String numNCorpBussUnitNo1 = reqX.getParameter("numNCorpBussUnitNo1");
+        String numNCorpBussAreaSqmts1 = reqX.getParameter("numNCorpBussAreaSqmts1");
+        String numNCorpBussCapitalization1 = reqX.getParameter("numNCorpBussCapitalization1");
+
+        String txtNCorpAct2 = reqX.getParameter("txtNCorpAct2");
+        String numNCorpBussUnitNo2 = reqX.getParameter("numNCorpBussUnitNo2");
+        String numNCorpBussAreaSqmts2 = reqX.getParameter("numNCorpBussAreaSqmts2");
+        String numNCorpBussCapitalization2 = reqX.getParameter("numNCorpBussCapitalization2");
+
+        String txtNCorpAct3 = reqX.getParameter("txtNCorpAct3");
+        String numNCorpBussUnitNo3 = reqX.getParameter("numNCorpBussUnitNo3");
+        String numNCorpBussAreaSqmts3 = reqX.getParameter("numNCorpBussAreaSqmts3");
+        String numNCorpBussCapitalization3 = reqX.getParameter("numNCorpBussCapitalization3");
+
+        String txtNCorpAct4 = reqX.getParameter("txtNCorpAct4");
+        String numNCorpBussUnitNo4 = reqX.getParameter("numNCorpBussUnitNo4");
+        String numNCorpBussAreaSqmts4 = reqX.getParameter("numNCorpBussAreaSqmts4");
+        String numNCorpBussCapitalization4 = reqX.getParameter("numNCorpBussCapitalization4");
+
+        String txtNCorpAct5 = reqX.getParameter("txtNCorpAct5");
+        String numNCorpBussUnitNo5 = reqX.getParameter("numNCorpBussUnitNo5");
+        String numNCorpBussAreaSqmts5 = reqX.getParameter("numNCorpBussAreaSqmts5");
+        String numNCorpBussCapitalization5 = reqX.getParameter("numNCorpBussCapitalization5");
+
+        String txtNCorpAct6 = reqX.getParameter("txtNCorpAct6");
+        String numNCorpBussUnitNo6 = reqX.getParameter("numNCorpBussUnitNo6");
+        String numNCorpBussAreaSqmts6 = reqX.getParameter("numNCorpBussAreaSqmts6");
+        String numNCorpBussCapitalization6 = reqX.getParameter("numNCorpBussCapitalization6");
+
+        String txtNCorpAct7 = reqX.getParameter("txtNCorpAct7");
+        String numNCorpBussUnitNo7 = reqX.getParameter("numNCorpBussUnitNo7");
+        String numNCorpBussAreaSqmts7 = reqX.getParameter("numNCorpBussAreaSqmts7");
+        String numNCorpBussCapitalization7 = reqX.getParameter("numNCorpBussCapitalization7");
+
+        Part fileNCorpLocSketchEst = reqX.getPart("fileNCorpLocSketchEst");
+        Part fileNCorpBrgyClear = reqX.getPart("fileNCorpBrgyClear");
+        Part fileNCorpSECreg = reqX.getPart("fileNCorpSECreg");
+        Part fileNCorpTitleProp = reqX.getPart("fileNCorpTitleProp");
+        Part fileNCorpLessorBussPermit = reqX.getPart("fileNCorpLessorBussPermit");
+        Part fileNCorpAuthLetterID = reqX.getPart("fileNCorpAuthLetterID");
+        // Hmmm
+        // Part fileNsingOthers = reqX.getPart("fileNsingOthers");
+        List<Part> fileNCorpOthers = reqX.getParts().stream().filter(part -> "fileNCorpOthers".equals(part.getName()))
+                .collect(Collectors.toList());
+
+        // Do the insert here if you can
+
     }
 }
