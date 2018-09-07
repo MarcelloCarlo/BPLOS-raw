7360d128cb2ece11fdf4bd825774c85684fdb8f4
John Carlo Gutierrez <jcgutierrez0102@gmail.com>
2018-09-07 23:18:59 +0800

    Scratch

--- src/com/qcapaeis/lguTransactions/updateNewAppEvaluationForm.java
+++ src/com/qcapaeis/lguTransactions/updateNewAppEvaluationForm.java
@@ -1,8 +1,14 @@
 package com.qcapaeis.lguTransactions;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 
 @WebServlet("/updateNewAppEvaluationForm")
-public class updateNewAppEvaluationForm {
+public class updateNewAppEvaluationForm extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	public updateNewAppEvaluationForm() {
+		super();
+	}
 
 }
