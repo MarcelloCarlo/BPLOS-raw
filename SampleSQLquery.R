12091b6f6fecb36f3c5dc3602d427aaff487c1f7
리 벤 <feralflare91@gmail.com>
2019-02-10 06:35:33 +0800

    r

--- SampleSQLquery.R
+++ SampleSQLquery.R
@@ -6,8 +6,11 @@ library(sqldf)
 require(RMySQL)
 
 
-#working 
+#working
+#Linux
 con <- dbConnect(RMySQL::MySQL(), host = "localhost",dbname="lgu_paeis_db",user = "root",unix.sock="/opt/lampp/var/mysql/mysql.sock")
+#Windows
+con <- dbConnect(RMySQL::MySQL(), host = "localhost",dbname="lgu_paeis_db",user = "root",unix.sock="/MysteryUnsolved/mysql.sock")
 
 #current working line
 dat <-dbSendQuery(con, 'SELECT SUM(OR_TOTAL_AMOUNT) AS G FROM bpls_t_official_receipt WHERE YEAR(OR_DATE) = 2019')
