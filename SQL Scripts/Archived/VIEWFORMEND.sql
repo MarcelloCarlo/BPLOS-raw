d97ab8ed1f7844adbaae85705c2707cd5dfa6e08
John Carlo Gutierrez <jcgutierrez0102@gmail.com>
2020-01-09 08:44:11 +0800

    File Cleanup

--- SQL Scripts/Archived/VIEWFORMEND.sql
+++
@@ -1,3 +0,0 @@
-DROP VIEW IF EXISTS view_applicaationformend;
-CREATE VIEW view_applicaationformend AS
-SELECT * FROM
--- SQL Scripts/Archived/VIEWROUTESL.sql
+++
@@ -1,6 +0,0 @@
-DROP VIEW IF EXISTS view_routeslip;
-
-CREATE VIEW view_routeslip AS
-  SELECT TL_DATE, TL_REMARKS, TL_DIV_NAME
-  FROM bpls_t_ap_history H
-         JOIN bpls_r_division DV ON H.TL_DIV_CODE = DV.DIV_CODE
